2f3f82d85c899b497755dc18d7bb0ce0a004ddc2
Thanh Lâm Trần <tranthanhlam2454.it@gmail.com>
2024-12-19 01:54:01 +0700

    done report

--- Practice/jest.config.ts
+++ Practice/jest.config.ts
@@ -1,27 +1,43 @@
-module.exports = {
-  preset: "ts-jest", // Cấu hình Jest sử dụng ts-jest
-  testEnvironment: "node", // Sử dụng môi trường Node.js
-  transform: {
-    "^.+\\.tsx?$": "ts-jest", // Biến tất cả file .ts và .tsx thành mã JavaScript
-  },
-  moduleFileExtensions: ["ts", "tsx", "js"], // Cấu hình các file extensions mà Jest sẽ nhận diện
-  testMatch: [
-    "**/?(*.)+(spec|test).ts", // Tìm kiếm các file test với đuôi .ts
+// import type { Config } from "@jest/types";
+
+// const config: Config.InitialOptions = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   reporters: [
+//     "default",
+//     "jest-allure-reporter", // Báo cáo Allure
+//   ],
+//   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Thiết lập Allure
+//   globals: {
+//     "ts-jest": {
+//       tsconfig: "<rootDir>/tsconfig.json",
+//     },
+//   },
+// };
+
+// export default config;
+
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "allure-jest/node",
+  reporters: [
+    "default", // Báo cáo mặc định
+    [
+      "jest-allure",
+      {
+        outputDirectory: "allure-results", // Thư mục kết quả Allure
+      },
+    ],
   ],
+  //setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Thiết lập thêm cho Allure
   globals: {
     "ts-jest": {
-      isolatedModules: true, // Tăng hiệu suất khi sử dụng Jest với TypeScript
+      tsconfig: "<rootDir>/tsconfig.json",
     },
   },
-  setupFiles: ["dotenv/config"], // Nếu bạn cần sử dụng các biến môi trường (nếu có)
-
-  // reporters: [
-  //   "default", // Báo cáo mặc định của Jest
-  //   [
-  //     "jest-allure-reporter",
-  //     {
-  //       resultsDir: "allure-results", // Thư mục lưu kết quả của Allure
-  //     },
-  //   ],
-  // ],
 };
+
+export default config;
+
--- Practice/tests/APItest.spec.ts
+++ Practice/tests/APItest.spec.ts
@@ -3,15 +3,19 @@ import BrandContribution from "../src/Page/BrandContribution";
 import * as allure from "allure-js-commons";
 
 
-
 const brandContribution = new BrandContribution();
 
 // This file contains test (expected result + data test)
 // Nghien cuu cach them hook cho test
-test("First API test", async () => {
 
+describe("Chec API", () => {it("Brand Contribution status's check", async () => {
+  await allure.epic("ECI");
+  await allure.feature("Market Insights");
+  await allure.suite("Brand Contribution");
+  await allure.severity("critical");
   const status = (await brandContribution.brandContributionOnShoptye()).status;
   await allure.tags("Test1", "Check 1");
-  expect(status).toBe(200);
 
-});
+  expect(status).toBe(200);
+});})
+
