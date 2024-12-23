792e9db85b2d4a6a63cf9b844b08e56e833975e3
Thanh Lâm Trần <tranthanhlam2454.it@gmail.com>
2024-12-24 01:55:35 +0700

    fix

--- Practice/tests/APItest.spec.ts
+++ Practice/tests/APItest.spec.ts
@@ -2,20 +2,35 @@ import { describe, expect, it, test } from "@jest/globals";
 import BrandContribution from "../src/Page/BrandContribution";
 import * as allure from "allure-js-commons";
 
-
 const brandContribution = new BrandContribution();
+const SECONDS = 1000;
+jest.setTimeout(10 * SECONDS);
 
 // This file contains test (expected result + data test)
 // Nghien cuu cach them hook cho test
 
-describe("Chec API", () => {it("Brand Contribution status's check", async () => {
-  await allure.epic("ECI");
-  await allure.feature("Market Insights");
-  await allure.suite("Brand Contribution");
-  await allure.severity("critical");
-  const status = (await brandContribution.brandContributionOnShoptye()).status;
-  await allure.tags("Test1", "Check 1");
+describe("Brand Test", () => {
+  it("Brand Contribution status's check", async () => {
+    await allure.epic("ECI");
+    await allure.feature("Market Insights");
+    await allure.suite("Brand Contribution");
+    await allure.severity("critical");
+    const status = (await brandContribution.brandContributionOnShoptye())
+      .status;
 
-  expect(status).toBe(200);
-});})
+    expect(status).toBe(200);
+  });
+});
 
+describe("Brand Test", () => {
+  it("Brand Contribution schema check", async () => {
+    await allure.epic("ECI");
+    await allure.feature("Market Insights");
+    await allure.suite("Brand Contribution");
+    await allure.severity("critical");
+    const status = (await brandContribution.brandContributionOnShoptye())
+      .status;
+
+    expect(status).toBe(200);
+  });
+});
