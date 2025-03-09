21ce64b8c7361a0b6bf142d6aeec96b9fb2735be
Thanh Lâm Trần <tranthanhlam2454.it@gmail.com>
2025-03-10 01:16:38 +0700

    add logger file

--- Practice/jest.setup.ts
+++ Practice/jest.setup.ts
@@ -1,7 +1,10 @@
-import "jest-extended"; // Nếu dùng thư viện hỗ trợ Jest matchers
-import "@testing-library/jest-dom"; // Nếu dùng React Testing Library
+// import "jest-extended"; // Nếu dùng thư viện hỗ trợ Jest matchers
+// import "@testing-library/jest-dom"; // Nếu dùng React Testing Library
+import fs from "fs";
+
 
 // Nếu cần matchers cho JSON schema
-import { matchers } from "jest-json-schema";
-expect.extend(matchers);
+// import { matchers } from "jest-json-schema";
+// expect.extend(matchers);
 
+fs.appendFileSync("jest.log", `Jest started at: ${new Date().toISOString()}\n`);
